47408d3cb839e71e83e8e57e3e6f9567223be331
G-Ray <g.bonneville@qwant.com>
2018-11-23 17:43:42 +0100

    Updates

---
+++ precache-manifest.80d36290ee6b7b7e333a66b875c96f08.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c6db4a2e14fd6e946d199f38510926b",
+    "url": "/masq-app/static/media/asap-latin-700.7c6db4a2.woff"
+  },
+  {
+    "revision": "f0cfbbab8cc2fca74d2e",
+    "url": "/masq-app/static/css/main.3fafe38b.chunk.css"
+  },
+  {
+    "revision": "d4c1276a463b507354622515fdcce068",
+    "url": "/masq-app/static/media/logout.d4c1276a.svg"
+  },
+  {
+    "revision": "fb48bf4166609c51024f",
+    "url": "/masq-app/static/js/1.fb48bf41.chunk.js"
+  },
+  {
+    "revision": "4339acbd2671eeee1026",
+    "url": "/masq-app/static/js/runtime~main.4339acbd.js"
+  },
+  {
+    "revision": "90269013cfe60f5f6fe0e38bacdd5b6d",
+    "url": "/masq-app/static/media/asap-latin-400.90269013.woff2"
+  },
+  {
+    "revision": "20495aa23daf3e541c6c19a85729d9ae",
+    "url": "/masq-app/static/media/asap-latin-400.20495aa2.woff"
+  },
+  {
+    "revision": "87a02118d118efbd8c75a30d4424e47a",
+    "url": "/masq-app/static/media/asap-latin-400italic.87a02118.woff2"
+  },
+  {
+    "revision": "bba1b4a3ecc109fad44534cdaea6e634",
+    "url": "/masq-app/static/media/asap-latin-500.bba1b4a3.woff2"
+  },
+  {
+    "revision": "65d145615910281f0ef14ba1f81f150c",
+    "url": "/masq-app/static/media/asap-latin-500.65d14561.woff"
+  },
+  {
+    "revision": "41264f825ce4cbac1f12a380291cf4d4",
+    "url": "/masq-app/static/media/asap-latin-600.41264f82.woff2"
+  },
+  {
+    "revision": "2c3ee5ad63d94580f78a9401f3e1deb4",
+    "url": "/masq-app/static/media/asap-latin-600.2c3ee5ad.woff"
+  },
+  {
+    "revision": "33f2a6e873d57cbcf5352482c1d9ee60",
+    "url": "/masq-app/static/media/asap-latin-600italic.33f2a6e8.woff2"
+  },
+  {
+    "revision": "a835e9ea118497fe4836af2959877e87",
+    "url": "/masq-app/static/media/asap-latin-500italic.a835e9ea.woff2"
+  },
+  {
+    "revision": "1e34f0625e0e1f0d02d4b2f9713327f4",
+    "url": "/masq-app/static/media/asap-latin-700.1e34f062.woff2"
+  },
+  {
+    "revision": "f0cfbbab8cc2fca74d2e",
+    "url": "/masq-app/static/js/main.f0cfbbab.chunk.js"
+  },
+  {
+    "revision": "7d030e1434133b8c04548bd18c87adae",
+    "url": "/masq-app/static/media/asap-latin-700italic.7d030e14.woff2"
+  },
+  {
+    "revision": "7cf06d21bed93dd3e1876ec9062ef0a3",
+    "url": "/masq-app/static/media/asap-latin-400italic.7cf06d21.woff"
+  },
+  {
+    "revision": "e877535c1ee2fe248d940d459f2138c3",
+    "url": "/masq-app/static/media/asap-latin-500italic.e877535c.woff"
+  },
+  {
+    "revision": "a860480843615954c506244c578293dd",
+    "url": "/masq-app/static/media/asap-latin-600italic.a8604808.woff"
+  },
+  {
+    "revision": "cdabf0089155e8595773c806e692d7a7",
+    "url": "/masq-app/static/media/asap-latin-700italic.cdabf008.woff"
+  },
+  {
+    "revision": "c061ba542fae938fb1674a60c0b9b3b9",
+    "url": "/masq-app/static/media/logo.c061ba54.svg"
+  },
+  {
+    "revision": "0b1d961911ebdc4be1f9748d4dfbffa0",
+    "url": "/masq-app/static/media/background.0b1d9619.svg"
+  },
+  {
+    "revision": "e518f82260c4acd798f66322d263bdf3",
+    "url": "/masq-app/static/media/plus-square.e518f822.svg"
+  },
+  {
+    "revision": "75a605b6847017480ec159ba36d1d141",
+    "url": "/masq-app/static/media/camera-square.75a605b6.svg"
+  },
+  {
+    "revision": "56b2bac8245b84b2f4b522d3d29b0a38",
+    "url": "/masq-app/static/media/close.56b2bac8.svg"
+  },
+  {
+    "revision": "ff3aa3d8b55ff7adfb392d12ba89e905",
+    "url": "/masq-app/static/media/cards.ff3aa3d8.svg"
+  },
+  {
+    "revision": "e486939ab1f0406b92b233835390c530",
+    "url": "/masq-app/static/media/phone.e486939a.svg"
+  },
+  {
+    "revision": "14755d326d8367a8ff3a4786ae4d0eb1",
+    "url": "/masq-app/static/media/settings.14755d32.svg"
+  },
+  {
+    "revision": "fb48bf4166609c51024f",
+    "url": "/masq-app/static/css/1.14fc7596.chunk.css"
+  },
+  {
+    "revision": "3cde24d4ed6e4953ed17676947163e68",
+    "url": "/masq-app/index.html"
+  }
+];
