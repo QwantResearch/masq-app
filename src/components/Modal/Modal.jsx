177ad30ba4c4d53a32c50841ae6d01b990625694
G-Ray <g.bonneville@qwant.com>
2019-01-29 17:03:12 +0100

    Reponsive modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,30 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import MediaQuery from 'react-responsive'
 
 import { ReactComponent as Close } from '../../assets/close.svg'
 
 import './Modal.scss'
 
-export default function Modal ({ onClose, width, height, children }) {
-  const modalStyle = {
-    height: height,
-    width: width
-  }
-
-  return (
-    <div className='Modal'>
-      <div className='overlay' onClick={onClose} />
-      <div className='modal' style={modalStyle}>
-        {onClose && <Close className='close' width={9} height={9} onClick={onClose} />}
-        {children}
-      </div>
+const Modal = ({ onClose, width, height, children }) => (
+  <div className='Modal'>
+    <div className='overlay' onClick={onClose} />
+    <div className='modal' style={{ width, height }}>
+      {onClose && <Close className='close' width={9} height={9} onClick={onClose} />}
+      {children}
     </div>
-  )
-}
+  </div>
+)
 
-Modal.propTypes = {
+const ResponsiveModal = ({ onClose, width, height, children }) => (
+  <MediaQuery maxDeviceWidth={900}>
+    {(matches) => {
+      if (matches) {
+        return <Modal width='100%' onClose={onClose} height={height} children={children} />
+      } else {
+        return <Modal width={width} onClose={onClose} height={height} children={children} />
+      }
+    }}
+  </MediaQuery>
+)
+
+Modal.propTypes =
+ResponsiveModal.propTypes = {
   onClose: PropTypes.func,
-  width: PropTypes.number,
+  width: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
   height: PropTypes.number,
   children: PropTypes.object
 }
+
+export default ResponsiveModal
