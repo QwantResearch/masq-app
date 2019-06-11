28278ddfe0c431ebc2d558c80bba9dbac887cbb8
G-Ray <g.bonneville@qwant.com>
2019-06-11 16:55:42 +0200

    UnsupportedBrowser modal: Add AlertOctagon icon

--- src/modals/UnsupportedBrowser/UnsupportedBrowser.jsx
+++ src/modals/UnsupportedBrowser/UnsupportedBrowser.jsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { Modal, Typography, Space } from '../../components'
 import { SUPPORTED_BROWSERS } from '../../lib/browser'
 import { capitalize } from '../../lib/utils'
+import { AlertOctagon } from 'react-feather'
 
 import styles from './UnsupportedBrowser.module.scss'
 
@@ -13,7 +14,9 @@ const UnsupportedBrowser = () => {
     <Modal width={511}>
       <div className={styles.UnsupportedBrowser}>
         <Typography type='title-modal'>{t('Browser not supported')}</Typography>
-        <Space size={32} />
+        <Space size={16} />
+        <AlertOctagon size={114} color={styles.colorRed} />
+        <Space size={16} />
         <Typography type='paragraph-modal'>{t('Your browser is not compatible with Masq. Please, try Masq with one of the compatible browsers:')}</Typography>
         <Space size={16} />
         {SUPPORTED_BROWSERS.map(browser => (
