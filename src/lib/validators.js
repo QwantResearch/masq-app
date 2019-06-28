81fd1680186f5151ad21b7661d30b7c8096c64e3
Levent DEMIR <levent.demir01@gmail.com>
2019-06-28 11:36:47 +0200

    remove isPassword, we allow any characters, and update special characters list

--- src/lib/validators.js
+++ src/lib/validators.js
@@ -2,8 +2,6 @@ const isName = str => /^$|^[A-zÀ-ú\- ]+$/.test(str)
 
 const isUsername = str => /^[\w!?$#@()\-*]+$/.test(str)
 
-const isPassword = str => /^([a-zA-Z\d!?$#%@()\\\-*_/@^+*&:<>:{};~'.|[\]])+$/.test(str)
-
 const getForce = str => {
   const info = getPasswordInfo(str)
   return str.length < 6 ? 0 : computeScore(info)
@@ -17,7 +15,7 @@ const containLowercase = str => /[a-z]/.test(str)
 
 const containNumber = str => /[0-9]/.test(str)
 
-const containSpecialCharacter = str => /[!?$#%@()\\\-*_/@^+*&:<>:{};~'.|[\]]/.test(str)
+const containSpecialCharacter = str => /[="!?$#%@()\\\-_/@^+*&:<>{};~'`.|[\]]/.test(str)
 
 const getPasswordInfo = str => ({
   lowercase: containLowercase(str),
