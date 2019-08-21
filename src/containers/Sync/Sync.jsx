d9c3fd8128920c196c69034cc34701248bcd7df8
G-Ray <g.bonneville@qwant.com>
2019-08-21 17:53:07 +0200

    Sync UI when pulling profile

--- src/containers/Sync/Sync.jsx
+++ src/containers/Sync/Sync.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 
+import { SyncDevice } from '../../modals'
 import SyncProfile from '../../lib/sync-profile'
 
-const Sync = () => {
+const Sync = ({ history }) => {
+  const [syncStep, setSyncStep] = useState('syncing')
+
   useEffect(() => {
     if (window.location.hash.substring(0, 7) !== '#/sync/') return
     const hash = window.location.hash.substr(7) // ignore #/sync/ characters
@@ -12,18 +16,31 @@ const Sync = () => {
 
     const startSync = async () => {
       const sp = new SyncProfile()
-      const [msg, channel, key] = JSON.parse(decoded)
 
-      if (msg !== 'pullProfile') return
-
-      await sp.init(channel, Buffer.from(key, 'base64'))
-      await sp.joinSecureChannel()
-      await sp.pullProfile()
+      try {
+        const [msg, channel, key] = JSON.parse(decoded)
+        if (msg !== 'pullProfile') throw new Error('Unexpected message')
+        await sp.init(channel, Buffer.from(key, 'base64'))
+        await sp.joinSecureChannel()
+        await sp.pullProfile()
+        setSyncStep('finished')
+      } catch (e) {
+        setSyncStep('error')
+      }
     }
+
     startSync()
   }, [])
 
-  return <div>Sync</div>
+  const onClick = () => {
+    history.push('/')
+  }
+
+  return <SyncDevice step={syncStep} onClick={onClick} />
+}
+
+Sync.propTypes = {
+  history: PropTypes.object.isRequired
 }
 
 export default Sync
