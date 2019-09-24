60424a438056ed88f92d5957f857b2a4636273b6
G-Ray <g.bonneville@qwant.com>
2019-09-24 14:59:08 +0200

    Scanner: Add crosshair

--- src/modals/Scanner/Scanner.jsx
+++ src/modals/Scanner/Scanner.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import QrReader from 'react-qr-reader'
+import { withTranslation } from 'react-i18next'
 
-import { Modal, Typography, Space } from '../../components'
+import { Modal, Typography, Space, Button } from '../../components'
 
 import styles from './Scanner.module.scss'
 
 class Scanner extends Component {
+  handleScan (url) {
+    if (url) {
+      window.open(url)
+    }
+  }
+
+  handleError (e) {
+    console.error(e)
+  }
+
   render () {
+    const { t, onClose } = this.props
     return (
-      <Modal onClose={this.props.onClose} padding={0} width={350}>
+      <Modal onClose={onClose} padding={0} width={350}>
+        <Space size={32} />
         <Typography type='title-modal'>Scan QR code</Typography>
         <Space size={32} />
         <div style={{ marginLeft: 16, marginRight: 16 }}>
@@ -17,6 +30,10 @@ class Scanner extends Component {
         </div>
         <Space size={32} />
         <div className={styles.Scanner}>
+          <div className={styles.crossHairTop} />
+          <div className={styles.crossHairRight} />
+          <div className={styles.crossHairBottom} />
+          <div className={styles.crossHairLeft} />
           <QrReader
             className={styles.reader}
             delay={300}
@@ -24,13 +41,17 @@ class Scanner extends Component {
             onScan={this.handleScan}
           />
         </div>
+        <Space size={32} />
+        <Button className={styles.button} color='neutral' width={185} onClick={onClose}>{t('go back')}</Button>
       </Modal>
     )
   }
 }
 
 Scanner.propTypes = {
+  t: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired
 }
 
-export default Scanner
+const translatedScanner = withTranslation()(Scanner)
+export default translatedScanner
