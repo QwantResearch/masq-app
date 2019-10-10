2b9f6b232be834809999efd31e138d997722d917
Levent DEMIR <levent.demir01@gmail.com>
2019-10-10 11:23:30 +0200

    Transform QRCOdeModal into class component

--- src/modals/QRCodeModal/QRCodeModal.jsx
+++ src/modals/QRCodeModal/QRCodeModal.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import QRCode from 'qrcode.react'
 import { Copy } from 'react-feather'
-import { useTranslation } from 'react-i18next'
+import { withTranslation } from 'react-i18next'
 import classNames from 'classnames'
 import { connect } from 'react-redux'
 
@@ -19,117 +19,134 @@ Pill.propTypes = {
   children: PropTypes.string.isRequired
 }
 
-const QRCodeModal = ({ onClose, profile }) => {
-  const { t } = useTranslation()
-  const [copied, setCopied] = useState(false)
-  const [link, setLink] = useState('')
-  const [syncStep, setSyncStep] = useState(null)
-  const [message, setMessage] = useState(null)
-  const [sp, setSp] = useState(null)
-
-  useEffect(() => {
-    const startSync = async () => {
-      const sp = new SyncProfile()
-      await sp.init()
-      setSp(sp)
-      const secureLink = await sp.getSecureLink()
-      setLink(secureLink)
-
-      try {
-        await sp.joinSecureChannel()
-        // Start Sync animation
-        setSyncStep('syncing')
-        const publicProfile = {
-          username: profile.username,
-          image: profile.image,
-          id: profile.id
-        }
-        const masq = getMasqInstance()
-        const db = masq.profileDB
-        await sp.pushProfile(db, profile.id, publicProfile)
-        setSyncStep('finished')
-      } catch (e) {
-        switch (e.message) {
-          case 'abort':
-            setMessage(t('The sync process has been aborted, please try again.'))
-            break
-          case 'alreadySynced':
-            setMessage(t('This profile is already synchronized on this device.'))
-            break
-          case 'usernameAlreadyExists':
-            setMessage(t('A profile with the same username already exists in this device, rename it and try again.'))
-            break
-          default:
-            setMessage(t('Error during the synchronization. Please retry.'))
-        }
-        setSyncStep('error')
-      }
+class QRCodeModal extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      copied: false,
+      link: '',
+      syncStep: null,
+      message: null
     }
-    startSync()
-  }, [])
+    this.sp = null
+    this.handleClose = this.handleClose.bind(this)
+  }
 
-  const copyLink = () => {
+  async startSync () {
+    const { t, profile } = this.props
+    this.sp = new SyncProfile()
+    await this.sp.init()
+    const secureLink = await this.sp.getSecureLink()
+    this.setState({ link: secureLink })
+
+    try {
+      await this.sp.joinSecureChannel()
+      // Start Sync animation
+      this.setState({ syncStep: 'syncing' })
+      const publicProfile = {
+        username: profile.username,
+        image: profile.image,
+        id: profile.id
+      }
+      const masq = getMasqInstance()
+      const db = masq.profileDB
+      await this.sp.pushProfile(db, profile.id, publicProfile)
+      this.setState({ syncStep: 'finished' })
+    } catch (e) {
+      switch (e.message) {
+        case 'abort':
+          this.setState({ message: t('The sync process has been aborted, please try again.') })
+          break
+        case 'alreadySynced':
+          this.setState({ message: t('This profile is already synchronized on this device.') })
+          break
+        case 'usernameAlreadyExists':
+          this.setState({ message: t('A profile with the same username already exists in this device, rename it and try again.') })
+          break
+        default:
+          this.setState({ message: t('Error during the synchronization. Please retry.') })
+      }
+      this.setState({ syncStep: 'error' })
+    }
+  }
+
+  copyLink () {
     const link = document.querySelector('#qrcode input')
     link.select()
     document.execCommand('copy')
-    setCopied(true)
-    setTimeout(() => setCopied(false), 3000)
+    this.setState({ copied: true })
+    setTimeout(() => this.setState({ copied: false }), 3000)
   }
 
-  const handleClose = () => {
-    sp.sw.close()
+  handleClose () {
+    const { onClose } = this.props
+    this.sp.sw.close()
     onClose()
   }
 
-  if (syncStep) {
-    return <SyncDevice step={syncStep} onClick={onClose} onClose={() => handleClose()} message={message} />
+  async componentDidMount () {
+    await this.startSync()
   }
 
-  return (
-    <Modal title={t('Add a device')} mobileHeader width={320} padding={78} onClose={onClose}>
-      <div id='qrcode' className={styles.QRCode}>
-        <Space size={32} />
-        <Typography maxWidth={320} type='paragraph-modal' align='center'>
-          {t('Scan this QR Code with the device you want to synchronize:')}
-        </Typography>
-        <Space size={18} />
-        <QRCode value={link} />
-        <Space size={26} />
-        <Typography
-          line
-          align='center'
-          type='paragraph-modal'
-          color={styles.colorBlueGrey}
-        >
-          {t('or')}
-        </Typography>
-        <Space size={22} />
-        <Typography maxWidth={320} type='paragraph-modal' align='center'>
-          {t(
-            'Copy the following link and paste it in the browser you want to use:'
-          )}
-        </Typography>
-        <Space size={24} />
-        <TextField
-          className={classNames(styles.input, { [styles.copied]: copied })}
-          readOnly
-          defaultValue={link}
-          button={<Copy style={{ height: 40 }} />}
-          height={36}
-          onClick={() => copyLink()}
-        />
-        <Space size={8} />
-        {copied && <Pill>{t('Link Copied !')}</Pill>}
-        {!copied && <Space size={28} />}
-        <Space size={16} />
+  render () {
+    const { link, copied, syncStep, message } = this.state
+    const { t, onClose } = this.props
+
+    return (
+      <div>
+        {
+          syncStep
+            ? <SyncDevice step={syncStep} onClick={onClose} onClose={() => this.handleClose()} message={message} />
+            : (
+              <Modal title={t('Add a device')} mobileHeader width={320} padding={78} onClose={() => this.handleClose()}>
+                <div id='qrcode' className={styles.QRCode}>
+                  <Space size={32} />
+                  <Typography maxWidth={320} type='paragraph-modal' align='center'>
+                    {t('Scan this QR Code with the device you want to synchronize:')}
+                  </Typography>
+                  <Space size={18} />
+                  <QRCode value={link} />
+                  <Space size={26} />
+                  <Typography
+                    line
+                    align='center'
+                    type='paragraph-modal'
+                    color={styles.colorBlueGrey}
+                  >
+                    {t('or')}
+                  </Typography>
+                  <Space size={22} />
+                  <Typography maxWidth={320} type='paragraph-modal' align='center'>
+                    {t(
+                      'Copy the following link and paste it in the browser you want to use:'
+                    )}
+                  </Typography>
+                  <Space size={24} />
+                  <TextField
+                    className={classNames(styles.input, { [styles.copied]: copied })}
+                    readOnly
+                    defaultValue={link}
+                    button={<Copy style={{ height: 40 }} />}
+                    height={36}
+                    onClick={() => this.copyLink()}
+                  />
+                  <Space size={8} />
+                  {copied && <Pill>{t('Link Copied !')}</Pill>}
+                  {!copied && <Space size={28} />}
+                  <Space size={16} />
+                </div>
+              </Modal>
+            )
+        }
       </div>
-    </Modal>
-  )
+    )
+  }
 }
 
 QRCodeModal.propTypes = {
   onClose: PropTypes.func,
-  profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
+  t: PropTypes.func
 }
 
 const mapStateToProps = state => ({
@@ -137,4 +154,5 @@ const mapStateToProps = state => ({
   devices: state.masq.devices
 })
 
-export default connect(mapStateToProps)(QRCodeModal)
+const translatedQRCodeModal = withTranslation()(QRCodeModal)
+export default connect(mapStateToProps)(translatedQRCodeModal)
